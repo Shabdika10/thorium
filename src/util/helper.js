559ba70149c73ca4e89290d286c96d173d51167e
shabdika pandey <pandeyshabdika@gmail.com>
2022-02-22 20:47:25 +0530

    my file

---
+++ src/util/helper.js
@@ -0,0 +1,37 @@
+// module.exports.date=function printDate(){
+//     const options = { 
+//         month: '2-digit', 
+//         day: '2-digit',
+//         year: 'numeric', 
+//       };
+      
+//       console.log(new Date().toLocaleDateString('en-US', options)); // mm/dd/yyyy 
+// }
+
+
+module.exports.date1= function mydate(date){
+  console.log(date)
+};
+
+module.exports.month=function printMonth(){
+//     var d = new Date();
+// var n = d.getMonth()+1;
+var d = new Date();
+var n = d.getMonth()+1;
+console.log(n);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
