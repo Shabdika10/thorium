e81d74f7b833e9e2e1fcf58be7b18a4730989a33
shabdika pandey <pandeyshabdika@gmail.com>
2022-03-02 03:20:56 +0530

    mongodb assignment

--- src/models/authorModel.js
+++ src/models/authorModel.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
-const bookSchema = new mongoose.Schema( {
-    bookName: String, 
-    author_id : String, 
-    tags: [String],
-    
-    isPublished: Boolean,
-    prices: String,
-    ratings: Number
+const authorSchema = new mongoose.Schema( {
+    author_id:{
+            type: Number,
+            required=true
+    } , 
+    author_name: String, 
+    age:Number,
+    address : String
+    // prices: String,
+    // ratings: Number
     // sales: {type: Number, default: 10}
     
     // " best boook on earth"   [ "Nodejs in detail" , "mongodb in detail", "fronend in detail"] 
@@ -22,4 +24,4 @@ const bookSchema = new mongoose.Schema( {
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
+module.exports = mongoose.model('Author', authorSchema) //users
